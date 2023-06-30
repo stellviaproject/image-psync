0677025c25ffee8a56d04beab8623627484f027c
Carlos Eduardo <stellviaanime@gmail.com>
2023-06-30 18:09:16 -0400

    Adding comments to function

--- parallel.go
+++ parallel.go
@@ -2,8 +2,11 @@ package psync
 
 import "sync"
 
+// It is use for passing to methods a function called on every region of image or slice in parallel.
+// The values minX, minY are the inferior limit of region, and maxX, maxY are the superior limit.
 type Action func(minX, minY, maxX, maxY int)
 
+// This function receive the width, height of image or slice and divide (width / parallel is the size of every region, only last region can have a diferent size) it by regions of the same sizes using parallel, then run action on the region.
 func ParallelRegionHorizontal(width, height, parallel int, action Action) {
 	wdx := width / parallel
 	wRegions := make([]int, parallel+1)
@@ -22,6 +25,7 @@ func ParallelRegionHorizontal(width, height, parallel int, action Action) {
 	wg.Wait()
 }
 
+// This function receive the width, height of image or slice and divide (height / parallel is the size of every region, only last region can have a diferent size) it by regions of the same sizes using parallel, then run action on the region.
 func ParallelRegionVertical(width, height, parallel int, action Action) {
 	hdy := height / parallel
 	hRegions := make([]int, parallel+1)
@@ -40,6 +44,7 @@ func ParallelRegionVertical(width, height, parallel int, action Action) {
 	wg.Wait()
 }
 
+// This function run action in parallel passing to action the coordinates of every window. The value of parallel control the number of gorutines.Note it's posible it doesn't cover every part of image, the number of windows in horizontal orientation will be number = width / window, but its division is an integer, then the last coordinates will be number * window and if width % window > 0 then an area of size = width % window won't be covered.
 func ParallelWindow(width, height, window, parallel int, action Action) {
 	prll := make(chan int, parallel)
 	wg := sync.WaitGroup{}
@@ -58,6 +63,7 @@ func ParallelWindow(width, height, window, parallel int, action Action) {
 	wg.Wait()
 }
 
+// This function run action in parallel for every pixel of image, the value of parallel control the number of gorutines. It is util when you want to process pixels around of every pixel.
 func ParallelForEach(width, height, parallel int, action Action) {
 	prll := make(chan int, parallel)
 	wg := sync.WaitGroup{}
@@ -75,6 +81,7 @@ func ParallelForEach(width, height, parallel int, action Action) {
 	wg.Wait()
 }
 
+// This function create a grid where every cell have size of size, only the last cells in the corner may have a diferent size. It's create gorutines for every cell, and the value of parallel control the number of gorutines.
 func ParallelGrid(width, height, size, parallel int, action Action) {
 
 	numX := width / size
